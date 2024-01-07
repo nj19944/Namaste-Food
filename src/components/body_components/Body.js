b92a8933c0ab11e779b76cb3ec74a89c8e75af7c
Nishant Jaiswal <nishant.jaiswal@asx.com.au>
2024-01-07 19:06:23 +0530

    Search and top rated restaurant code been pushed aby fetching api data

--- src/components/body_components/Body.js
+++ src/components/body_components/Body.js
@@ -1,20 +1,57 @@
-import {useState} from "react";
-import restaurants from "../../utils/Data";
+import {useEffect, useState} from "react";
+import { SWIGGY_API } from "../../utils/constants";
 import RestaurantCard from "./RestaurantCard";
 import './Body.css'
+import Shimmer from "./Shimmer";
 
 const Body = () =>{
 
-    const [listOfRestaurants, setListOfRestraunt] = useState(restaurants);
+    const [listOfRestaurants, setListOfRestraunt] = useState([]);
 
-    return (
+    const [filteredlistOfRestaurants, setFilteredListOfRestraunt] = useState([]);
+
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    const fetchData = async() => 
+    {
+        const data = await fetch(SWIGGY_API);
+        const json = await  data.json();
+
+        //console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setListOfRestraunt(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredListOfRestraunt(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    return (listOfRestaurants.length === 0) ? <Shimmer/> :  (
             <div className="body">
                 <div className="filter">
+                    <div className = "search">
+                        <input type = "text" 
+                        className="search-box" 
+                        value = {searchText} 
+                        onChange = {(e) => setSearchText(e.target.value)}
+                        />
+                        <button 
+                            onClick={() => {
+                                const filteredRestaurant = listOfRestaurants.filter((res) => 
+                                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                                )
+                                
+                                setFilteredListOfRestraunt(filteredRestaurant);
+                            }}
+                        >
+                            Search
+                        </button>
+                    </div>
                     <button 
                     className="filter-btn"
                     onClick = { () => {
-                        const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4.5);
-                        setListOfRestraunt(filteredList);
+                        const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4.2);
+                        setFilteredListOfRestraunt(filteredList);
                     }}
                     >
                         Top Rated Restaurants
@@ -22,7 +59,7 @@ const Body = () =>{
                 </div>
                 <div className="res-container">
                     {
-                    listOfRestaurants.map((restaurant) => (
+                    filteredlistOfRestaurants.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} resData = {restaurant} />
                     ))
                     } 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
